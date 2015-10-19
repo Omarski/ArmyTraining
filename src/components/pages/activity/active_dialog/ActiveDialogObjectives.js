2d1a1feb7054032f2837172b27159cefb884b526
Curtis <cwingert@alelo.com>
2015-10-19 05:30:26 -0700

    Active Dialog Work

--- src/components/pages/activity/active_dialog/ActiveDialogObjectives.js
+++ src/components/pages/activity/active_dialog/ActiveDialogObjectives.js
@@ -11,8 +11,19 @@ var ActiveDialogObjectiveActions = require('../../../../actions/active_dialog/Ac
 
 function getCompState() {
 
+    var objectives = [];
+    if (ActiveDialogObjectiveStore.data() && ActiveDialogObjectiveStore.data().objectives) {
+        var arr = ActiveDialogObjectiveStore.data().objectives;
+        var len = arr.length;
+        for (var i = 0; i < len; i++) {
+            var obj = arr[i];
+            if (obj.label && obj.label !== "") {
+                objectives.push(obj);
+            }
+        }
+    }
     return {
-        objectives: ActiveDialogObjectiveStore.data() || []
+        objectives: objectives
     };
 }
 
@@ -38,18 +49,13 @@ var ActiveDialogObjectives = React.createClass({
 
     render: function() {
 
-        var _self = this;
-
         var objectivesList = <ListGroupItem />;
 
         if (this.state.objectives && this.state.objectives.length > 0) {
             objectivesList = this.state.objectives.map(function(item, index) {
-                var name = item.objectives[0].realizations[0].anima;
                 return  <ListGroupItem key={index}>
-                    <a className="" href="#" data-animation-name={name} onClick={_self.hintAction.bind(_self, item)}>
-                        {item.act}
-                    </a>
-                </ListGroupItem>
+                            {item.label}
+                        </ListGroupItem>
             });
         }
 
