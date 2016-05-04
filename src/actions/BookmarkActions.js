fffa878ceaa16ce1a5c1ca46b1aa0e66017580c0
Curtis <cwingert@alelo.com>
2016-05-04 11:21:59 -0700

    Bug 22268 - Global -- Implement automatic page-level bookmarking

--- src/actions/BookmarkActions.js
+++ src/actions/BookmarkActions.js
@@ -6,14 +6,22 @@ var BookmarkActions = {
     /**
      * @param  {string} text
      */
+    setCurrent: function(data) {
+        AppDispatcher.dispatch({
+            actionType: BookmarkConstants.BOOKMARK_SET_CURRENT,
+            data: data
+        });
+    },
+
+    /**
+     * @param  {string} text
+     */
     create: function(data) {
         AppDispatcher.dispatch({
             actionType: BookmarkConstants.BOOKMARK_CREATE,
             data: data
         });
     },
-
-
 
     /**
      * @param  {string} id
--- src/components/BreadcrumbsView.js
+++ src/components/BreadcrumbsView.js
@@ -4,6 +4,12 @@ var PageActions = require('../actions/PageActions');
 var BookmarkActions = require('../actions/BookmarkActions');
 var BookmarkStore = require('../stores/BookmarkStore');
 var NotificationActions = require('../actions/NotificationActions');
+var ReactBootstrap = require('react-bootstrap');
+var OverlayTrigger = ReactBootstrap.OverlayTrigger;
+var Button = ReactBootstrap.Button;
+var Popover = ReactBootstrap.Popover;
+var ListGroup = ReactBootstrap.ListGroup;
+var ListGroupItem = ReactBootstrap.ListGroupItem;
 
 function getPageState() {
     var page = null;
@@ -18,14 +24,28 @@ function getPageState() {
         chapterTitle = PageStore.chapter().title;
         pageTitle = PageStore.page().title;
 
+        var bm = {
+            unit: PageStore.unit().data.xid,
+            chapter: PageStore.chapter().xid,
+            page: PageStore.page().xid
+        };
+
+        setTimeout(function() {
+            BookmarkActions.setCurrent(bm);
+        });
+
+
     }
 
-    if (BookmarkStore.bookmark() &&
-        BookmarkStore.bookmark().page &&
+    if (BookmarkStore.current() &&
+        BookmarkStore.current().page &&
         page &&
-        (BookmarkStore.bookmark().page === page.xid)) {
+        (BookmarkStore.current().page === page.xid)) {
         bookmarked = true;
     }
+
+
+
 
     return {
         pageTitle: pageTitle,
@@ -40,7 +60,8 @@ var BreadcrumbsView = React.createClass({
         var bm = {
             unit: PageStore.unit().data.xid,
             chapter: PageStore.chapter().xid,
-            page: PageStore.page().xid
+            page: PageStore.page().xid,
+            title: PageStore.page().title
         };
 
         NotificationActions.show({
@@ -53,6 +74,12 @@ var BreadcrumbsView = React.createClass({
         this.setState(getPageState());
 
     },
+
+    bookmarkSelected: function(bm) {
+        console.log(bm)
+        PageActions.jump(bm);
+    },
+
     getInitialState: function() {
         var pageState = getPageState();
         return pageState;
@@ -71,6 +98,45 @@ var BreadcrumbsView = React.createClass({
     },
     render: function() {
 
+        var self = this;
+        var bookmarks = BookmarkStore.bookmarks();
+        var items = "";
+        if (bookmarks) {
+            items = bookmarks.map(function(item) {
+                return (<ListGroupItem>
+                    <a href="#" onClick={self.bookmarkSelected.bind(self, item)}>{item.title}</a>
+                </ListGroupItem>)
+            });
+        }
+
+        var popover =   <Popover id="bookmarksPopover" title='Bookmarks'>
+            <ListGroup>
+                <Button
+                    id="breadcrumbsButton"
+                    type="button"
+                    className="btn btn-default btn-block btn-action"
+                    onClick={this.bookmark}
+                >
+                    Bookmark This Page
+                </Button>
+            </ListGroup>
+            <ListGroup>
+                {items}
+            </ListGroup>
+        </Popover>;
+
+        var bookmarkBtn = (
+            <OverlayTrigger trigger='click' placement='left' overlay={popover}>
+                <Button
+                    id="breadcrumbsButton"
+                    type="button"
+                    className={("btn btn-default btn-link main-nav-bookmark ") + ((this.state.bookmarked) ? "selected" : "")}
+                >
+                    <span className="glyphicon glyphicon-bookmark" aria-hidden="true"></span>
+                </Button>
+            </OverlayTrigger>
+        );
+
         return (
             <div>
                 <ol className="breadcrumb main-breadcrumbs">
@@ -78,17 +144,7 @@ var BreadcrumbsView = React.createClass({
                     <li><a href="#" >{this.state.chapterTitle}</a></li>
                     <li><a href="#" className="active">{this.state.pageTitle}</a></li>
                 </ol>
-
-
-                <button
-                    id="breadcrumbsButton"
-                    type="button"
-                    className={("btn btn-default btn-link main-nav-bookmark ") + ((this.state.bookmarked) ? "selected" : "")}
-                    onClick={this.bookmark}
-                    >
-                    <span className="glyphicon glyphicon-bookmark" aria-hidden="true"></span>
-                </button>
-
+                {bookmarkBtn}
             </div>
 
         );
