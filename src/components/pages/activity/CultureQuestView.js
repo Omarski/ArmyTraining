6b61c5c3d5c1896cbc71f50de3d443e486ca4a33
Omar Amer <omargravity@gmail.com>
2016-05-11 15:06:14 -0700

    Culture Quest canvas solution

--- src/components/pages/activity/CultureQuestView.js
+++ src/components/pages/activity/CultureQuestView.js
@@ -84,15 +84,16 @@ var CultureQuestView = React.createClass({
 
 var CultureQuestMap = React.createClass({
     getInitialState: function() {
-        return {regions:null};
+        return {}; //regions:null
     },
 
     componentWillMount: function() {
-        this.placeRegions();
+        //this.placeRegions();
         //PageStore.addChangeListener(this._onChange);
     },
 
     componentDidMount: function() {
+        this.placeRegions();
         //PageStore.addChangeListener(this._onChange);
     },
 
@@ -100,30 +101,44 @@ var CultureQuestMap = React.createClass({
         //PageStore.removeChangeListener(this._onChange);
     },
 
+    createCanvas: function(canvasData){
+
+        var canv = document.createElement("canvas");
+        canv.setAttribute('width',canvasData.canvasWidth);
+        canv.setAttribute('height',canvasData.canvasHeight);
+        canv.setAttribute('id',canvasData.canvasId);
+        canv.style = canvasData.canvasStyle;
+        canv.class = "cultureQuest-region-canvas";
+        var context = canv.getContext("2d");
+        var image = new Image();
+        image.src = canvasData.mapSrc;
+        image.onload = function(){
+            context.drawImage(image,0,0);
+        }
+
+        return canv;
+    },
+
     placeRegions: function(){
 
         var self = this;
+        var canvasColl = [];
+        
+        //loop through regions //var regions =
+        this.props.mapData.regions.map(function(region,index){
+            
+            var regionCanvas =  self.createCanvas({
+                                canvasWidth:region.nearWidth,
+                                canvasHeight:region.nearHeight,
+                                canvasId:"cultureQuest_canvas_" + index,
+                                canvasStyle:"{z-index:"+index+1+"}",
+                                mapSrc: self.props.mediaPath + region.image
+            });
 
-        //loop through regions
-        var regions = this.props.mapData.regions.map(function(region,index){
+            document.getElementById("cultureQuest-map").appendChild(regionCanvas);
 
-            var imgStyle = {position: "absolute", top: region.xMap+"px", left: region.yMap+"px",
-                width: region.nearWidth+"px", height: region.nearHeight+"px"};
-            console.log("Region " + index);
-            return (
-                <CultureQuestRegion key={region.name}
-                                    className="cultureQuest-region"
-                                    src={self.props.mediaPath + region.image}
-                                    regionStyle={imgStyle}
-                                    regionName={region.name}
-                                    regionClick={self.onRegionClick}
-                                    regionOver={self.onRegionOver}
-                />
-            )
+            canvasColl.push(regionCanvas);
         });
-
-        self.setState({regions:regions});
-        //return regions;
     },
 
     onRegionClick: function(e) {
@@ -140,44 +155,44 @@ var CultureQuestMap = React.createClass({
         var mapData = self.props.mapData;
         var mapBackground = self.props.mediaPath + mapData.mapBackground;
         var mapStyle = {background:"#000 url("+mapBackground+") no-repeat 100% 100%", width:"768px" , height:"504px", textAlign:"center"};
-
+        //{self.state.regions}
         return (
-            <div className="cultureQuest-map" style={mapStyle}>
-                {self.state.regions}
+            <div id="cultureQuest-map" className="cultureQuest-map" style={mapStyle} ref={(c) => self._mainMap = c}>
+
             </div>
         );
     }
 });
 
-var CultureQuestRegion = React.createClass({
-    getInitialState: function() {
-        return {};
-    },
-
-    componentWillMount: function() {
-        //PageStore.addChangeListener(this._onChange);
-    },
-
-    componentDidMount: function() {
-        //PageStore.addChangeListener(this._onChange);
-    },
-
-    componentWillUnmount: function() {
-        //PageStore.removeChangeListener(this._onChange);
-    },
-    render: function() {
-        var self = this;
-
-        return (
-            <img className="cultureQuest-region" src={self.props.imgSrc}
-                 src={self.props.src}
-                 style={self.props.regionStyle}
-                 name={self.props.regionName}
-                 onClick={self.props.regionClick}
-                 onMouseOver={self.props.regionOver} >
-            </img>
-        );
-    }
-});
+// var CultureQuestRegion = React.createClass({
+//     getInitialState: function() {
+//         return {};
+//     },
+//
+//     componentWillMount: function() {
+//         //PageStore.addChangeListener(this._onChange);
+//     },
+//
+//     componentDidMount: function() {
+//         //PageStore.addChangeListener(this._onChange);
+//     },
+//
+//     componentWillUnmount: function() {
+//         //PageStore.removeChangeListener(this._onChange);
+//     },
+//     render: function() {
+//         var self = this;
+//
+//         return (
+//             <img className="cultureQuest-region" src={self.props.imgSrc}
+//                  src={self.props.src}
+//                  style={self.props.regionStyle}
+//                  name={self.props.regionName}
+//                  onClick={self.props.regionClick}
+//                  onMouseOver={self.props.regionOver} >
+//             </img>
+//         );
+//     }
+// });
 
 module.exports = CultureQuestView;
