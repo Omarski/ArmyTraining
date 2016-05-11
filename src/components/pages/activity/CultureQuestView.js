e33f75054fc08fa7c485ecaa92e54e3dc9c410e6
Omar Amer <omargravity@gmail.com>
2016-05-10 18:51:18 -0700

    culture quest functional map

--- src/components/pages/activity/CultureQuestView.js
+++ src/components/pages/activity/CultureQuestView.js
@@ -3,17 +3,27 @@
  */
 var React = require('react');
 var PageStore = require('../../../stores/PageStore');
-
+var PageHeader = require('../../widgets/PageHeader');
 
 function getPageState(props) {
-    var title = "";
-    var pageType = "";
-    var noteItems = "";
-    var mediaItems = "";
+
+    var data = {
+        page: "",
+        sources: [],
+        title: "",
+        pageType: "",
+        showQuiz: false,
+        showPop: false,
+        mediaPath: "data/media/"
+    } ;
+
 
     if (props && props.page) {
-        title = props.page.title;
-        pageType = props.page.type;
+        data.title = props.page.title;
+        data.pageType = props.page.type;
+        data.page = props.page;
+        data.mapData = JSON.parse(data.page.info.property[2].value);
+        //for (var s in data.page) console.log(s + " : " + data.page[s]);
 
         if (props.page.note) {
 
@@ -24,16 +34,12 @@ function getPageState(props) {
         }
     }
 
-    return {
-        title: title,
-        note: noteItems,
-        media: mediaItems,
-        pageType: pageType
-    };
+    return data;
 }
 
 var CultureQuestView = React.createClass({
     getInitialState: function() {
+
         var pageState = getPageState(this.props);
         return pageState;
     },
@@ -49,14 +55,20 @@ var CultureQuestView = React.createClass({
     componentWillUnmount: function() {
         //PageStore.removeChangeListener(this._onChange);
     },
+
     render: function() {
         var self = this;
+        var state = self.state;
+        var page = self.state.page;
+        var title = self.state.title;
+        var sources = self.state.sources;
+
         return (
             <div>
-                <PageHeader sources={state.sources} title={title} key={this.state.page.xid}/>
-                <CultureQuestMap />
+                <PageHeader sources={sources} title={title} key={state.page.xid}/>
+                <CultureQuestMap mediaPath={state.mediaPath} mapData={state.mapData} />
                 {self.state.showQuiz? <CultureQuestQuiz />:null}
-                {self.state.showQuiz? <CultureQuestPopup />:null}
+                {self.state.showPop? <CultureQuestPopup />:null}
                 
             </div>
         );
@@ -69,10 +81,77 @@ var CultureQuestView = React.createClass({
     }
 });
 
+
 var CultureQuestMap = React.createClass({
     getInitialState: function() {
-        var pageState = getPageState(this.props);
-        return pageState;
+        return {regions:null};
+    },
+
+    componentWillMount: function() {
+        this.placeRegions();
+        //PageStore.addChangeListener(this._onChange);
+    },
+
+    componentDidMount: function() {
+        //PageStore.addChangeListener(this._onChange);
+    },
+
+    componentWillUnmount: function() {
+        //PageStore.removeChangeListener(this._onChange);
+    },
+
+    placeRegions: function(){
+
+        var self = this;
+
+        //loop through regions
+        var regions = this.props.mapData.regions.map(function(region,index){
+
+            var imgStyle = {position: "absolute", top: region.xMap+"px", left: region.yMap+"px",
+                width: region.nearWidth+"px", height: region.nearHeight+"px"};
+            console.log("Region " + index);
+            return (
+                <CultureQuestRegion key={region.name}
+                                    className="cultureQuest-region"
+                                    src={self.props.mediaPath + region.image}
+                                    regionStyle={imgStyle}
+                                    regionName={region.name}
+                                    regionClick={self.onRegionClick}
+                                    regionOver={self.onRegionOver}
+                />
+            )
+        });
+
+        self.setState({regions:regions});
+        //return regions;
+    },
+
+    onRegionClick: function(e) {
+        console.log("clicked on : " + e.target.name);
+    },
+
+    onRegionOver: function(e) {
+        console.log("Hovering over : " + e.target.name);
+    },
+
+
+    render: function() {
+        var self = this;
+        var mapData = self.props.mapData;
+        var mapBackground = self.props.mediaPath + mapData.mapBackground;
+        var mapStyle = {background:"#000 url("+mapBackground+") no-repeat 100% 100%", width:"768px" , height:"504px", textAlign:"center"};
+
+        return (
+            <div className="cultureQuest-map" style={mapStyle}>
+                {self.state.regions}
+            </div>
+        );
+    }
+});
+
+var CultureQuestRegion = React.createClass({
+    getInitialState: function() {
+        return {};
     },
 
     componentWillMount: function() {
@@ -88,10 +167,15 @@ var CultureQuestMap = React.createClass({
     },
     render: function() {
         var self = this;
-        return (
-            <div>
 
-            </div>
+        return (
+            <img className="cultureQuest-region" src={self.props.imgSrc}
+                 src={self.props.src}
+                 style={self.props.regionStyle}
+                 name={self.props.regionName}
+                 onClick={self.props.regionClick}
+                 onMouseOver={self.props.regionOver} >
+            </img>
         );
     }
 });
