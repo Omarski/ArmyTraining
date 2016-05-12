323820f63983bdbfe49650c5164a2d8244b0e636
Omar Amer <omargravity@gmail.com>
2016-05-11 19:15:27 -0700

    Culture Quest region detection

--- src/components/pages/activity/CultureQuestView.js
+++ src/components/pages/activity/CultureQuestView.js
@@ -84,7 +84,7 @@ var CultureQuestView = React.createClass({
 
 var CultureQuestMap = React.createClass({
     getInitialState: function() {
-        return {}; //regions:null
+        return {canvasColl:[]};
     },
 
     componentWillMount: function() {
@@ -103,18 +103,26 @@ var CultureQuestMap = React.createClass({
 
     createCanvas: function(canvasData){
 
+        var self = this;
         var canv = document.createElement("canvas");
         canv.setAttribute('width',canvasData.canvasWidth);
         canv.setAttribute('height',canvasData.canvasHeight);
         canv.setAttribute('id',canvasData.canvasId);
         canv.style = canvasData.canvasStyle;
-        canv.class = "cultureQuest-region-canvas";
+        canv.className = "cultureQuest-region-canvas";
         var context = canv.getContext("2d");
         var image = new Image();
         image.src = canvasData.mapSrc;
         image.onload = function(){
             context.drawImage(image,0,0);
-        }
+        };
+        canv.addEventListener("click", function(e){
+            self.onRegionClick(e);
+        });
+
+        canv.addEventListener("mousemove", function(e){
+            self.onRegionOver(e);
+        });
 
         return canv;
     },
@@ -123,10 +131,10 @@ var CultureQuestMap = React.createClass({
 
         var self = this;
         var canvasColl = [];
-        
-        //loop through regions //var regions =
-        this.props.mapData.regions.map(function(region,index){
-            
+
+        //loop through regions
+        self.props.mapData.regions.map(function(region,index){
+
             var regionCanvas =  self.createCanvas({
                                 canvasWidth:region.nearWidth,
                                 canvasHeight:region.nearHeight,
@@ -139,60 +147,90 @@ var CultureQuestMap = React.createClass({
 
             canvasColl.push(regionCanvas);
         });
+
+            self.setState({canvasColl:canvasColl});
+    },
+
+    reorderRegionStack: function(e) {
+
+        var self = this;
+        var canvasId = e.target.getAttribute('id');
+        console.log("pixel hit on: " + canvasId);
+        for (var i=0; i< self.state.canvasColl.length; i++){
+            if (self.state.canvasColl[i].getAttribute('id') === canvasId) {
+                self.state.canvasColl[i].style.pointerEvents = "auto";
+                //self.state.canvasColl[i].style.zIndex = 2;
+                //console.log("matched: " + self.state.canvasColl[i].getAttribute('id') + " With: " + canvasId);
+            } else {
+                console.log("to z-index 1");
+                self.state.canvasColl[i].style.pointerEvents = "none";
+                //self.state.canvasColl[i].style.zIndex = 1;
+            }
+        }
+
     },
 
     onRegionClick: function(e) {
-        console.log("clicked on : " + e.target.name);
+        this.pixelTracker(e, "click");
     },
 
     onRegionOver: function(e) {
-        console.log("Hovering over : " + e.target.name);
+        this.pixelTracker(e, "hover");
     },
 
+    pixelTracker: function(e, mode) {
+
+        var self = this;
+        // utility function for finding the position of the clicked pixel
+        function findPos(obj) {
+
+            var curleft = 0, curtop = 0;
+            if (obj.offsetParent) {
+                do {
+                    curleft += obj.offsetLeft;
+                    curtop += obj.offsetTop;
+                } while (obj = obj.offsetParent);
+                return {x: curleft, y: curtop};
+            }
+            return undefined;
+        }
+
+        // get the position of clicked pixel
+        var pos = findPos(e.target);
+        var x = e.pageX - pos.x;
+        var y = e.pageY - pos.y;
+        // get reference to canvas element clicked on
+        var canvas = e.target.getContext('2d');
+        // return array of [RED,GREEN,BLUE,ALPHA] as 0-255 of clicked pixel
+        var pixel = canvas.getImageData(x, y, 1, 1).data;
+        // if the alpha is not 0, we clicked a non-transparent pixel
+        // could be easily adjusted to detect other features of the clicked pixel
+        // if (pixel[3] != 0) {
+        //     alert("Clicked the dice!");
+        // }
+        if (mode == "hover"){
+            console.log("pixel[3]: " + pixel[3]);
+            if (pixel[3] != 0) {
+                  self.reorderRegionStack(e);
+            }else {e.target.style.zIndex = 1; }
+        }else if (mode == "click"){
+            console.log("clicked on: " + e.target.getAttribute('id'));
+        }
+    },
 
     render: function() {
         var self = this;
         var mapData = self.props.mapData;
         var mapBackground = self.props.mediaPath + mapData.mapBackground;
-        var mapStyle = {background:"#000 url("+mapBackground+") no-repeat 100% 100%", width:"768px" , height:"504px", textAlign:"center"};
-        //{self.state.regions}
+        var mapStyle = {background:"#000 url("+mapBackground+") no-repeat 100% 100%",
+            position:"relative", width:"768px" , height:"504px", textAlign:"center"};
+
         return (
-            <div id="cultureQuest-map" className="cultureQuest-map" style={mapStyle} ref={(c) => self._mainMap = c}>
+            <div id="cultureQuest-map" className="cultureQuest-map" style={mapStyle}>
 
             </div>
         );
     }
 });
-
-// var CultureQuestRegion = React.createClass({
-//     getInitialState: function() {
-//         return {};
-//     },
-//
-//     componentWillMount: function() {
-//         //PageStore.addChangeListener(this._onChange);
-//     },
-//
-//     componentDidMount: function() {
-//         //PageStore.addChangeListener(this._onChange);
-//     },
-//
-//     componentWillUnmount: function() {
-//         //PageStore.removeChangeListener(this._onChange);
-//     },
-//     render: function() {
-//         var self = this;
-//
-//         return (
-//             <img className="cultureQuest-region" src={self.props.imgSrc}
-//                  src={self.props.src}
-//                  style={self.props.regionStyle}
-//                  name={self.props.regionName}
-//                  onClick={self.props.regionClick}
-//                  onMouseOver={self.props.regionOver} >
-//             </img>
-//         );
-//     }
-// });
 
 module.exports = CultureQuestView;
