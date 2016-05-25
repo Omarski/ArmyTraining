0a27e710ce1396f612a30902e2ed15b2af945d33
ghernandez <ghernandez@alelo.com>
2016-05-24 21:46:44 -0700

    First version of the quiz results page. Needs more work. Needs to: - get text from a store - get score cutoff ranges from a store - send event to store if the user has passed the quiz
    Plus more work.

--- src/components/pages/activity/QuizView.js
+++ src/components/pages/activity/QuizView.js
@@ -9,20 +9,99 @@ var PageHeader = require('../../widgets/PageHeader');
 
 function getPageState(props) {
     var data = {
+        answers: [],
         title: "",
+        pageFeedback: "",
+        pageFeedback1: "Your score was {0}%. You may want to review the section and try this quiz again.", // TODO <--- should be moved to store and localization text
+        pageFeedback2: "Your score was {0}%. You're on the right track! I can see that you are working hard to \
+        remember all of the cultural information you have learned. However, your quiz score does not permit you to move \
+        on to the next section. Please take a few minutes to review this section. When you feel that you're ready, try the quiz again.", // TODO <--- should be moved to store and localization text
+        pageFeedback3: "Your score was {0}%. Congratulations! You have successfully completed this section. You may now \
+        move on to the next section in your course of instruction.", // TODO <--- should be moved to store and localization text
         pageType: "",
         page: "",
-        sources: []
-    };
+        sources: [],
+        scorePercent: 0
+    }
 
     if (props && props.page) {
         data.title = props.page.title;
         data.pageType = props.page.type;
         data.page = props.page;
+    }
+
+    // get all quiz pages for this section
+    var quizPages = PageStore.getChapterQuizPages();
+    var quizPagesIter = 0;
+    var quizPagesPassed = 0;
+
+    // for each quiz page check if it was complete
+    while(quizPagesIter < quizPages.length) {
+
+        // get quiz page
+        var quizPage = quizPages[quizPagesIter];
+
+        // increment
+        quizPagesIter++;
+
+        // get quizPage answer object
+        if (quizPage.state && quizPage.state.answer) {
+            var quizAnswer = quizPage.state.answer;
+            var answer;
+            var passed;
+            var question;
+
+            // get answer
+            if (quizAnswer.answer) {
+                answer = quizAnswer.answer
+            }
+
+            // get question
+            if (quizAnswer.question) {
+                question = quizAnswer.question;
+            }
+
+            // check if passed
+            if (quizAnswer.passed === true) {
+                quizPagesPassed++;
+                passed = true;
+
+            } else {
+                passed = false;
+            }
+
+            data.answers.push({answer: answer, passed: passed, question: question});
+        }
 
     }
 
+
+    // calculate score
+    var feedback = "";
+    if (quizPages.length > 0) {
+        data.scorePercent = Math.round((quizPagesPassed / quizPages.length) * 100);
+
+        // get feedback text
+        if (data.scorePercent === 100) {
+            feedback = data.pageFeedback3;
+        } else if (data.scorePercent >= 80) {
+            feedback = data.pageFeedback2;
+        } else {
+            feedback = data.pageFeedback1;
+        }
+    } else {
+        feedback = data.pageFeedback1;
+    }
+
+
+    // update text
+    data.pageFeedback = replaceScoreText(data.scorePercent, feedback);
+
     return data;
+}
+
+function replaceScoreText(score, text) {
+    return text.replace("{0}", score.toString());
 }
 
 var QuizView = React.createClass({
@@ -32,11 +111,10 @@ var QuizView = React.createClass({
     },
 
     componentWillMount: function() {
-
+        PageStore.addChangeListener(this._onChange);
     },
 
     componentDidMount: function() {
-        //PageStore.addChangeListener(this._onChange);
         $('[data-toggle="tooltip"]').tooltip();
     },
 
@@ -51,20 +129,41 @@ var QuizView = React.createClass({
     componentWillUnmount: function() {
 
     },
+
     render: function() {
         var self = this;
         var state = self.state;
         var title = state.title;
         var pageType = state.pageType;
 
+        // render rows
+        var questionsRows = this.state.answers.map(function(item, index) {
+            return (
+                <QuizAnswerRow answer={item.answer} passed={item.passed} key={index} question={item.question}/>
+            )
+        });
 
         return (
             <div>
                 <PageHeader sources={state.sources} title={title} key={this.state.page.xid}/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-12">
+                            <div className="quiz-feedback">
+                                {state.pageFeedback}
+                            </div>
 
+                            <table className="table table-bordered quiz-feedback-table">
+                                <QuizTableHeader/>
+                                {questionsRows}
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     },
+
     /**
      * Event handler for 'change' events coming from the BookStore
      */
@@ -73,6 +172,48 @@ var QuizView = React.createClass({
             this.setState(getPageState(this.props));
         }
 
+    }
+});
+
+var QuizTableHeader = React.createClass({
+   render: function() {
+       return (
+           <tr className="">
+               <th>Correct</th>
+               <th>Question</th>
+               <th>Answer</th>
+           </tr>
+       );
+   }
+});
+
+
+var QuizAnswerRow = React.createClass({
+    render: function() {
+
+        var answer = this.props.answer;
+        var passed = this.props.passed;
+        var question = this.props.question;
+        var className = "glyphicon glyphicon-remove-circle quiz-feedback-icon quiz-feedback-icon-incorrect"
+
+        // changed if passed
+        if (passed) {
+            className = "glyphicon glyphicon-ok-circle quiz-feedback-icon quiz-feedback-icon-correct"
+        }
+
+        return (
+            <tr className="">
+                <td>
+                    <div className={className}/>
+                </td>
+                <td>
+                    {question}
+                </td>
+                <td>
+                    {answer}
+                </td>
+            </tr>
+        );
     }
 });
 
