001afe7299fc6a9ebc599e5206c90fb620ebe74c
ghernandez <ghernandez@alelo.com>
2016-06-09 16:43:45 -0700

    Updated with new info tags used in the questionnaire.

--- src/constants/InfoTagConstants.js
+++ src/constants/InfoTagConstants.js
@@ -1,9 +1,14 @@
 module.exports = {
+    INFO_PROP_GROUPID: "exclusivegroupid",
+    INFO_PROP_HIDDEN: "hidden",
     INFO_PROP_MINILESSONID: "minilessonID",
     INFO_PROP_NOSHUFFLE: "noshuffle",
+    INFO_PROP_PLAYLIST: "playlist",
     INFO_PROP_PROLOGUE: "prologue",
     INFO_PROP_QUESTIONNAIRE: "questionnaire",
     INFO_PROP_QUIZ: "quiz",
     INFO_PROP_QUIZPAGE: "quizpage",
-    INFO_PROP_RECOMMENDED: "recommended"
+    INFO_PROP_RADIOBUTTON: "radiobutton",
+    INFO_PROP_RECOMMENDED: "recommended",
+    INFO_PROP_REQUIRED: "required"
 };
