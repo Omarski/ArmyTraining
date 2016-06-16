62cbe07cc0222fcb6e1e3ee37d72bc8874e77c72
ghernandez <ghernandez@alelo.com>
2016-06-15 17:12:01 -0700

    Added new store actions and view for the pre test and post test.

---
+++ src/actions/PrePostTestActions.js
@@ -0,0 +1,165 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var InfoTagConstants = require("../constants/InfoTagConstants");
+var LocalizationStore = require('../stores/LocalizationStore');
+var PageConstants = require('../constants/PageConstants');
+var PageTypeConstants = require('../constants/PageTypeConstants');
+var PrePostTestConstants = require('../constants/PrePostTestConstants');
+var PrePostTestStore = require('../stores/PrePostTestStore');;
+var UnitStore = require('../stores/UnitStore');
+var Utils = require('../components/widgets/Utils');
+
+
+var PrePostTestActions = {
+
+    build: function() {
+
+        var foundPages = [];
+        var pretestchapter = null;
+        var posttestchapter = null;
+
+        // reset any past values
+        AppDispatcher.dispatch({
+            actionType: PrePostTestConstants.PRE_POST_TEST_RESET
+        });
+
+        // get all the units
+        var units = UnitStore.getAll();
+
+        // iterate over each unit
+        for (var key in units) {
+            var unit = units[key];
+
+            // check which units are marked required
+            if (UnitStore.isRequired(unit.id)) {
+                if (unit.data.chapter) {
+                    // iterate over the units chapters
+                    for (var chapterIndex = 0; chapterIndex < unit.data.chapter.length; chapterIndex++) {
+                        var chapter = unit.data.chapter[chapterIndex];
+
+                        // check for pretest chapter, save reference and skip it
+                        if (Utils.findInfo(chapter.info, InfoTagConstants.INFO_PROP_PRETEST) !== null) {
+                            pretestchapter = chapter;
+                        }
+
+                        // check for posttest chapter, save reference and skip it
+                        if (Utils.findInfo(chapter.info, InfoTagConstants.INFO_PROP_POSTTEST) !== null) {
+                            posttestchapter = chapter;
+                            break;
+                        }
+
+                        if (chapter.pages) {
+                            // iterate over the chapters pages
+                            for (var pageIndex = 0; pageIndex < chapter.pages.length; pageIndex++) {
+                                var page = chapter.pages[pageIndex]
+                                // if page is a quiz page and is marked for the preposttest then save it for later
+                                if (page.state && (page.state.quizpage == true) && (page.preposttest === true)) {
+                                    foundPages.push({
+                                        page: page,
+                                        chapterId: chapter.xid,
+                                        chapterTitle: chapter.title,
+                                        unitId: unit.data.xid,
+                                        unitTitle: unit.data.title
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // iterate over found pages
+        var preTestCount = 0;
+        var postTestCount = 0;
+
+        for (var i in foundPages) {
+            var page = foundPages[i].page;
+            var chapterId = foundPages[i].chapterId;
+            var chapterTitle = foundPages[i].chapterTitle;
+            var unitId = foundPages[i].unitId;
+            var unitTitle = foundPages[i].unitTitle;
+
+            // if found a pre test chapter then add the page to it
+            if (pretestchapter) {
+                // increment count
+                preTestCount++;
+
+                // use JSON to clone object
+                var pageCopyPre = JSON.parse(JSON.stringify(page));
+
+                // change title
+                pageCopyPre.title = LocalizationStore.labelFor("pretest", "lblTitle", [preTestCount]);
+
+                // update xid as a combination of chapter id and page id
+                pageCopyPre.xid = chapterId + "_" + page.xid;
+
+                // add pre post test attribute
+                pageCopyPre.preposttest = true;
+
+                // add page to either the end or right before the quiz end page if found
+                var insertIndexPre = pretestchapter.pages.length;
+                if (pretestchapter.pages.length > 0) {
+                    var lastPage = pretestchapter.pages[pretestchapter.pages.length - 1];
+                    if (lastPage.type === PageTypeConstants.TEST_OUT_QUIZ_END) {
+                        insertIndexPre = -1;
+                    }
+                }
+
+                // insert it to the lesson
+                pretestchapter.pages.splice(insertIndexPre, 0, pageCopyPre);
+            }
+
+            // if found a post test chapter then add the page to it
+            if (posttestchapter) {
+                // increment count
+                postTestCount++;
+
+                // use JSON to clone object
+                var pageCopyPost = JSON.parse(JSON.stringify(page));
+
+                // change title
+                pageCopyPost.title = LocalizationStore.labelFor("pretest", "lblTitle", [postTestCount]);
+
+                // update xid as a combination of chapter id and page id
+                pageCopyPost.xid = chapterId + "_" + page.xid;
+
+                // add pre post test attribute
+                pageCopyPost.preposttest = true;
+
+                // add page to either the end or right before the quiz end page if found
+                var insertIndexPost = posttestchapter.pages.length;
+                if (posttestchapter.pages.length > 0) {
+                    var lastPage = posttestchapter.pages[posttestchapter.pages.length - 1];
+                    if (lastPage.type === PageTypeConstants.POST_TEST_QUIZ_END) {
+                        insertIndexPost = -1;
+                    }
+                }
+
+                // insert it to the lesson
+                posttestchapter.pages.splice(insertIndexPost, 0, pageCopyPost);
+            }
+
+
+            // add to store
+            AppDispatcher.dispatch({
+                actionType: PrePostTestConstants.PRE_POST_TEST_ADD_PAGE,
+                data: {
+                    pageId: page.xid,
+                    chapterId: chapterId,
+                    chapterTitle: chapterTitle,
+                    unitId: unitId,
+                    unitTitle: unitTitle
+                }
+            });
+        }
+    },
+
+    markUnitComplete: function() {
+        AppDispatcher.dispatch({
+            actionType: "nuttin"
+        });
+    }
+
+};
+
+module.exports = PrePostTestActions;
---
+++ src/components/pages/content/TestOutQuizEndView.js
@@ -0,0 +1,228 @@
+var React = require('react');
+var PageActions = require('../../../actions/PageActions');
+var PageStore = require('../../../stores/PageStore');
+var SettingsStore = require('../../../stores/SettingsStore');
+var PageHeader = require('../../widgets/PageHeader');
+var PrePostTestActions = require('../../../actions/PrePostTestActions');
+var PrePostTestStore = require('../../../stores/PrePostTestStore');
+var LocalizationStore = require('../../../stores/LocalizationStore');
+
+
+function getPageState(props) {
+    var data = {
+        answers: [],
+        bAccepted: true,
+        bPassed: false,
+        title: "",
+        pageFeedback: "",
+        pageType: "",
+        page: "",
+        sources: [],
+    }
+
+    if (props && props.page) {
+        data.title = props.page.title;
+        data.pageType = props.page.type;
+        data.page = props.page;
+    }
+
+    var testOutUnits = {};
+    var testOutUnitsOrder = [];  // dont like this
+
+    // get all quiz pages for this section
+    var quizPages = PageStore.getChapterQuizPages();
+
+    for (var i = 0; i < quizPages.length; i++) {
+        // get page
+        var page = quizPages[i];
+        var pageId = quizPages[i].xid;
+
+        // get original unit id and title of the page
+        var unitId = PrePostTestStore.getUnitIdByPageId(pageId);
+        var unitTitle = PrePostTestStore.getUnitTitleByPageId(pageId);
+
+        // check if already added
+        if (!testOutUnits[unitId]) {
+            testOutUnits[unitId] = {passed: true, title: unitTitle, id: unitId};
+            testOutUnitsOrder.push(unitId);
+        }
+
+        // if only 1 is not passed mark unit as not eligable to test out
+        // TODO change so cutoff value can be authored
+        if (!page.state || !page.state.answer || page.state.answer.passed !== true) {
+            testOutUnits[unitId].passed = false;
+        }
+    }
+
+    // compile results and feedback
+    data.pageFeedback = LocalizationStore.labelFor("pretest", "lblFailed");
+
+    var orderLength = testOutUnitsOrder.length;
+    while(orderLength--) {
+        var id = testOutUnitsOrder[orderLength];
+        if (testOutUnits[id]) {
+            data.answers.unshift(testOutUnits[id]);
+
+            // if at least one was passed then change feedback
+            if (testOutUnits[id].passed === true) {
+                data.bPassed = true;
+                data.pageFeedback = LocalizationStore.labelFor("pretest", "lblPassed");
+            }
+        }
+    }
+
+    return data;
+}
+
+
+function TestOutComplete(state) {
+    // if passed and accepted then mark units completed
+    if (state.bPassed && state.bAccepted) {
+
+        // gather units that are passed
+        console.log(state);
+
+        // mark unit complete
+
+        // mark the chapters in the units as complete
+        PrePostTestActions.markUnitComplete();
+    }
+    //PrePostTestActions.markUnitComplete();
+}
+
+
+var TestOutQuizEndView = React.createClass({
+    getInitialState: function() {
+        var pageState = getPageState(this.props);
+
+        setTimeout(function() {
+            PageActions.markChapterComplete();
+        });
+
+        return pageState;
+    },
+
+    componentWillMount: function() {
+        PageStore.addChangeListener(this._onChange);
+    },
+
+    componentDidMount: function() {
+        $('[data-toggle="tooltip"]').tooltip();
+    },
+
+    componentWillUnmount: function() {
+        PageStore.removeChangeListener(this._onChange);
+
+        TestOutComplete(this.state);
+    },
+
+    acceptSelected: function() {
+        this.state.bAccepted = true;
+    },
+
+    declinedSelected: function() {
+        this.state.bAccepted = true;
+    },
+
+    render: function() {
+        var self = this;
+        var state = self.state;
+        var title = state.title;
+        var passed = state.bPassed;
+
+        // compile feedback
+        var passedRow = "";
+        if (passed || 1) {
+            passedRow = (
+                <div className="row">
+                    <div className="col-lg-4">
+                        <input name="radioGroup" type="radio" className="multiple-choice-checkbox" defaultChecked onChange={this.acceptSelected}>{LocalizationStore.labelFor("pretest", "lblCreditAccept")}</input>
+                    </div>
+                    <div className="col-lg-4">
+                        <input name="radioGroup" type="radio" className="multiple-choice-checkbox" onChange={this.declinedSelected}>{LocalizationStore.labelFor("pretest", "lblCreditDecline")}</input>
+                    </div>
+                </div>
+            );
+        }
+
+        // render rows
+        var testOutRows = this.state.answers.map(function(item, index) {
+            return (
+                <TestOutRow key={index} title={item.title} passed={item.passed}/>
+            )
+        });
+
+        return (
+            <div>
+                <PageHeader sources={state.sources} title={title} key={this.state.page.xid}/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-12">
+                            <div className="quiz-feedback">
+                                {state.pageFeedback}
+                            </div>
+                        </div>
+                    </div>
+                    {passedRow}
+                    <div className="row">
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <table className="table table-bordered table-striped">
+                                <TestOutTableHeader/>
+                                {testOutRows}
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        );
+    },
+
+    /**
+     * Event handler for 'change' events coming from the BookStore
+     */
+    _onChange: function() {
+        if (this.isMounted()) {
+            this.setState(getPageState(this.props));
+        }
+
+    }
+});
+
+var TestOutTableHeader = React.createClass({
+    render: function() {
+        return (
+            <tr>
+                <th className="text-center">{LocalizationStore.labelFor("pretest", "headerTitle")}</th>
+                <th className="text-center">{LocalizationStore.labelFor("pretest", "headerTestOut")}</th>
+            </tr>
+        );
+    }
+});
+
+
+var TestOutRow = React.createClass({
+    render: function() {
+        var passed = this.props.passed;
+        var title = this.props.title;
+        var className = "glyphicon glyphicon-remove-circle quiz-feedback-icon quiz-feedback-icon-incorrect"
+
+        // changed if passed
+        if (passed) {
+            className = "glyphicon glyphicon-ok-circle quiz-feedback-icon quiz-feedback-icon-correct"
+        }
+
+        return (
+            <tr>
+                <td>
+                    {title}
+                </td>
+                <td>
+                    <span className={className}/>
+                </td>
+            </tr>
+        );
+    }
+});
+
+module.exports = TestOutQuizEndView;
