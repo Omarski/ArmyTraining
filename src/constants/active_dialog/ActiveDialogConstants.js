ebea7572ad996c840baff05bf79b7b98df74ccd8
ghernandez <ghernandez@alelo.com>
2016-07-04 15:10:06 -0700

    Added new ActiveDialog constants for upcoming updates.

--- src/constants/active_dialog/ActiveDialogConstants.js
+++ src/constants/active_dialog/ActiveDialogConstants.js
@@ -1,10 +1,16 @@
 var keyMirror = require('keymirror');
 
 module.exports = keyMirror({
+    ACTIVE_DIALOG_ACTION_BLOCKING: null,
+    ACTIVE_DIALOG_ACTION_COA_SET: null,
+    ACTIVE_DIALOG_ACTION_COMPLETE: null,
+    ACTIVE_DIALOG_ACTION_OUTPUT: null,
+    ACTIVE_DIALOG_CONTINUE_DIALOG: null,
     ACTIVE_DIALOG_CREATE: null,
     ACTIVE_DIALOG_DESTROY: null,
     ACTIVE_DIALOG_LOAD: null,
-    ACTIVE_DIALOG_HANDLE_INPUT: null, // when option is selected
+    ACTIVE_DIALOG_HANDLE_INPUT: null,   // when option is selected
     ACTIVE_DIALOG_SET_ACTIVE_COA: null, // when hint is selected
-    ACTIVE_DIALOG_HINTS_SHOWN: null // when hints are shown to user from button click
+    ACTIVE_DIALOG_START_DIALOG: null,   // when intro is closed
+    ACTIVE_DIALOG_HINTS_SHOWN: null     // when hints are shown to user from button click
 });
