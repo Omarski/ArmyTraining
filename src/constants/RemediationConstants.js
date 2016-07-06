4e237e2b64a31470407d17255b7c3bf7ea041264
ghernandez <ghernandez@alelo.com>
2016-07-05 19:44:09 -0700

    Added first commit of the remediation view. This should be able to be used in other places outside of the ActiveDialogs. All in needs is to be passed a list of page to load. Needs styling

---
+++ src/constants/RemediationConstants.js
@@ -0,0 +1,9 @@
+var keyMirror = require('keymirror');
+
+module.exports = keyMirror({
+    REMEDIATION_CREATE: null,
+    REMEDIATION_DESTROY: null,
+    REMEDIATION_LOAD_COMPLETE: null,
+    REMEDIATION_LOAD_NEXT: null,
+    REMEDIATION_LOAD_PREV: null
+});
