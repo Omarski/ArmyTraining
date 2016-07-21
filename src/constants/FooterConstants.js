c492a364ba0aea177856f7ae1c68c4267f2e39fd
ghernandez <ghernandez@alelo.com>
2016-07-20 21:13:00 -0700

    Added footer store, actions and constants. We need to be able to enable and disabled the next buttons. We now can!

---
+++ src/constants/FooterConstants.js
@@ -0,0 +1,10 @@
+var keyMirror = require('keymirror');
+
+module.exports = keyMirror({
+    FOOTER_DISABLE_ALL: null,
+    FOOTER_DISABLE_NEXT: null,
+    FOOTER_DISABLE_PREV: null,
+    FOOTER_ENABLE_ALL: null,
+    FOOTER_ENABLE_NEXT: null,
+    FOOTER_ENABLE_PREV: null
+});
