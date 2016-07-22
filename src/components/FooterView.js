4c3ad0da6a99b05123e9a4bda55a4c3d3fed7c74
Curtis <cwingert@alelo.com>
2016-07-22 10:37:46 -0700

    explorer btn alignment fix

--- src/components/FooterView.js
+++ src/components/FooterView.js
@@ -250,7 +250,10 @@ var FooterView = React.createClass({
                         className="btn btn-default btn-lg btn-link btn-text-icon btn-clk"
                         aria-label={this.state.expanded ? LocalizationStore.labelFor("footer", "tooltipIndexCollapse") : LocalizationStore.labelFor("footer", "tooltipIndexExpand")}
                         onClick={this.toggleTOC}>
-                    <span id="lessonsIndexBtnIcon" className={this.state.expanded ? "glyphicon glyphicon-circle-arrow-down btn-icon" : "glyphicon glyphicon-circle-arrow-up btn-icon"} aria-hidden="true"></span><span className="explorer-link">{LocalizationStore.labelFor("footer", "lblExplorer")}</span>
+                    <div style={{'height':'25px', 'lineHeight':'21px'}}>
+                        <span style={{'verticalAlign':'middle'}} id="lessonsIndexBtnIcon" className={this.state.expanded ? "glyphicon glyphicon-circle-arrow-down btn-icon" : "glyphicon glyphicon-circle-arrow-up btn-icon"} aria-hidden="true"></span>
+                        <span style={{'verticalAlign':'middle'}} className="explorer-link">{LocalizationStore.labelFor("footer", "lblExplorer")}</span>
+                    </div>
                 </button>
             );
             progressView = (
