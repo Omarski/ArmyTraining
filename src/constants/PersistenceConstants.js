743e6d8aa456a7f87d4ce7f625db7eb32ff9a575
ghernandez <ghernandez@alelo.com>
2016-07-31 13:49:28 -0700

    Added new constants for adding bookmarking to the persistence store

--- src/constants/PersistenceConstants.js
+++ src/constants/PersistenceConstants.js
@@ -2,6 +2,8 @@ var keyMirror = require('keymirror');
 
 module.exports = keyMirror({
     PERSISTENCE_REMOVE: null,
+    PERSISTENCE_REMOVE_BOOKMARK: null,
     PERSISTENCE_SET: null,
+    PERSISTENCE_SET_BOOKMARK: null,
     PERSISTENCE_SET_STORAGE_TYPE: null
 });
